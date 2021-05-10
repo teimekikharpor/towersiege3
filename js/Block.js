7b10cd93c2673dbe8f0a9bedf48faa798771f2c7
teimekikharpor <77444056+teimekikharpor@users.noreply.github.com>
2021-05-10 20:08:44 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -11,15 +11,7 @@ class Block{
         this.width = width;
         this.height = height;
         World.add(world, this.body);
-        blocks1.score();
-        blocks2.score();
-        blocks3.score();
-        blocks4.score();
-        blocks5.score();
-        blocks6.score();
-        blocks7.score();
-        blocks8.score();
-        blocks9.score();
+        
       }
       display(){
         if(this.body.speed < 3){
